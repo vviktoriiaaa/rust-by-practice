f2d0905af0af0a335cd3e29d14a93bb121b64c57
vviktoriiaaa <viktoriia.moskva@e-u.edu.ua>
2026-02-22 13:13:55 +0100

    Delete ex3.rs

--- ex3.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    let x: i32 = 10;
-    let y: i32 = 5; // Винесли y зовні блоку
-    {
-        println!("Inner scope value of x is {} and value of y is {}", x, y);
-    }
-    println!("Outer scope value of x is {} and value of y is {}", x, y);
-}
